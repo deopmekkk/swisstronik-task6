3d6b12c38718929396fb998eb055502d54ca4661
deopmekkk <deopmek@gmail.com>
2024-08-04 05:58:36 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x6d19cb54b1B5BFd16f02be7027892842b05F9926'
+export const ProxyAdmin = '0x048D630AF2d7b7930D61aa7F01c053FE55a475E8'
+export const SWTRImplementation = '0xf82120F612F0A5fbb1419c65d114706e78f8ec15'
     
